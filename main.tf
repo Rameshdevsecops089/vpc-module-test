171392fac991978ee36c4919d97b6ef6bfb1dfc7
Rameshdevsecops089 <drameshdevops089@gmail.com>
2025-07-03 13:16:55 +0530

    data added

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 module "vpc" {
-    source = "git::https://github.com/Rameshdevsecops089/vpc-module-test.git" # this is user githug 
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/daws-84s/terraform-aws-vpc.git?ref=main"
+    /* project = "roboshop"
+    environment = "dev"
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
     project = var.project
     environment = var.environment
     public_subnet_cidrs = var.public_subnet_cidrs
